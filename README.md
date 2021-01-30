a1f875309d3e30fe9a6fc15767f55263a6874c1b
Pieter E Smit <pieter.smit@vigor.nz>
2021-01-30 19:17:41 +1300

    Add arguments and update READE. md with set of gauge thumbnails

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
 # python-gauge
 
-[andrewshay.me Blog Post - How to create a gauge / speedometer graphic with Python](https://andrewshay.me/blog/how-to-create-a-gauge-speedometer-graphic-with-python/)  
-Create a gauge graphic with Python.  
-Images files (including psd) licensed under [Attribution 4.0 International (CC BY 4.0)](https://creativecommons.org/licenses/by/4.0/)
+
+# History
+
+ * 2021-used example from andrewshay https://github.com/Andrew-Shay/python-gauge as an starting point to draw a gauge
+ * P.Smit Add new image for Gauge and Needle.
+ * P.Smit Allow for multiple outputs to be generated.
+ * P.Smit Add commandline arguments.
+ * P.Smit add --thumbnail option.
+ * P.Smit add --debug flag
 
 # Setup
 
-`Python 3.6`  
-`pip install -r requirements`
+`Python3 -m pip install -r requirements`
+
+# Run
+
+python3 python_guage.py
+ * Generates 10 gauges from 0% to 100%
+
 
 # Screenshot
 
-![screenshot](gauge_thumbnail.png)
+![10%](images/new_gauge_thumb_10%.png)
+![20%](images/new_gauge_thumb_20%.png)
+![30%](images/new_gauge_thumb_30%.png)
+![40%](images/new_gauge_thumb_40%.png)
+![50%](images/new_gauge_thumb_50%.png)
+![60%](images/new_gauge_thumb_60%.png)
+![70%](images/new_gauge_thumb_70%.png)
+![80%](images/new_gauge_thumb_80%.png)
+![90%](images/new_gauge_thumb_90%.png)
+![100%](images/new_gauge_thumb_100%.png)
--- python_gauge.py
+++ python_gauge.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 """
+Copyright (C) 2021 P Smit
 Copyright (C) 2018 FireEye, Inc., created by Andrew Shay. All Rights Reserved.
 """
 
@@ -8,14 +10,33 @@ from PIL import Image, ImageDraw
 import math
 import os
 import glob
+import argparse
 
-debug = False
+def parseArgs():
+    p = argparse.ArgumentParser(description='Draw some gauges.',
+                                formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    p.add_argument('percentages', metavar='N%', type=int, nargs='*',
+                        default=list(range(0, 101, 10)),
+                        help='percentages to draw the needle at')
+    p.add_argument('--thumbnails', action='store_true',
+                        help='generate thumbnails')
+    p.add_argument('--gaugeDegrees', metavar="N°", type=int, default=180+90,
+                    help="degrees the needle spans on gauge, assumed to be symetric around North.")
+    p.add_argument('--debug', action='store_true',
+                        help='print debug messages.')
+    #Note: use vars(p.parse_args()) to turn into dict
+    return p.parse_args()
+
+
+args = parseArgs()
+debug = args.debug
+if debug: print(f"Debug: {args=}")
 
 # percentages = [ 0, 25, 50, 75, 100 ]  # Percent for gauge
-percentages = range(0, 101, 10)
+percentages = args.percentages
 
-output_file_name_template = 'new_gauge_{percent}%.png'
-fileList = glob.glob(output_file_name_template.format(percent="*"))
+output_file_name_template = 'new_gauge{extra}{percent}%.png'
+fileList = glob.glob(output_file_name_template.format(extra="_", percent="*"))
 for filePath in fileList: os.remove(filePath)
 
 fnImgNeedle = 'needle-red.png'
@@ -32,7 +53,7 @@ locCalcPlaceNeedle = (
 
 for percent in percentages:
     fraction = percent / 100
-    gaugeDegrees = 180 + 90
+    gaugeDegrees = args.gaugeDegrees
     rotation = ( gaugeDegrees ) * fraction  # 180 degrees because the gauge is half a circle
     rotation = gaugeDegrees / 2 - rotation  # Factor in the needle graphic pointing to 50 (90 degrees)
 
@@ -46,11 +67,19 @@ for percent in percentages:
 
 
 
-    print(f"Debug {[item1 + item2 for item1, item2 in zip(locGaugePlaceNeedle, locNeedleRotate)]} {fnImgNeedle}:{dial.size} {fnImgGauge}:{gauge.size} place:{locCalcPlaceNeedle} rotation:{rotation}")
+    if debug: print(f"Debug {[item1 + item2 for item1, item2 in zip(locGaugePlaceNeedle, locNeedleRotate)]} {fnImgNeedle}:{dial.size} {fnImgGauge}:{gauge.size} place:{locCalcPlaceNeedle} rotation:{rotation}")
     gauge.paste(dial, locCalcPlaceNeedle, mask=dial)  # Paste needle onto gauge
 
     gaugeDraw = ImageDraw.Draw(gauge)
-    print(f"DEBUG:: {locGaugePlaceNeedle[0]-50}, {locGaugePlaceNeedle[0]+200}")
+    if debug: print(f"DEBUG:: {locGaugePlaceNeedle[0]-50}, {locGaugePlaceNeedle[0]+200}")
     gaugeDraw.text((locGaugePlaceNeedle[0]-10, locGaugePlaceNeedle[0]+120), f"%{percent}", outline='red', fill='blue')
 
-    gauge.save(output_file_name_template.format(percent=percent))
+    if args.thumbnails:
+        MAX_SIZE = (30, 30)
+        gauge.thumbnail(MAX_SIZE)
+        fn=output_file_name_template.format(extra="_thumb_", percent=percent)
+        gauge.save()
+    else:
+        fn=output_file_name_template.format(extra="_", percent=percent)
+        gauge.save(fn)
+    print(f"Created: {fn}")
